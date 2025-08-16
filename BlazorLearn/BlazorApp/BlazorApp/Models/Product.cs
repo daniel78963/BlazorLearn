73b2453949ddac2e507a52a13931f792051d6188
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-08-16 11:57:31 -0500

    Add product

--- BlazorLearn/BlazorApp/BlazorApp/Models/Product.cs
+++ BlazorLearn/BlazorApp/BlazorApp/Models/Product.cs
@@ -2,5 +2,13 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        // Navigation property for related Product_Prop entities
+        public List<Product_Prop> Properties { get; set; } = new List<Product_Prop>();
     }
 }
