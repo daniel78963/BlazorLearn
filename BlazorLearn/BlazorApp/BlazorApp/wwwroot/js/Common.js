bd526ea2af1af9a401cb9c2a0d564ce151e2b7df
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-09-17 18:42:54 -0500

    toastr

---
+++ BlazorLearn/BlazorApp/BlazorApp/wwwroot/js/Common.js
@@ -0,0 +1,12 @@
+﻿window.showToastr = function (type, message, title) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+    if (type == "warning") {
+        toastr.warning(message);
+    }
+    
+}
