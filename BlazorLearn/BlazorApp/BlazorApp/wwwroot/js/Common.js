5076025ea09d65ab7ff3e62674a0d805ae74746c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-09-19 18:35:04 -0500

    Sweet Alert

--- BlazorLearn/BlazorApp/BlazorApp/wwwroot/js/Common.js
+++ BlazorLearn/BlazorApp/BlazorApp/wwwroot/js/Common.js
@@ -8,5 +8,23 @@
     if (type == "warning") {
         toastr.warning(message);
     }
-    
+
+}
+
+window.ShowSwal = function (type, message, title) {
+    if (type == "success") {
+        Swlal.fire({
+            icon: 'success',
+            title: title,
+            text: message
+        });
+    }
+
+    if (type == "error") {
+        Swlal.fire({
+            icon: 'error',
+            title: title,
+            text: "Something is not valid"
+        });
+    }
 }
