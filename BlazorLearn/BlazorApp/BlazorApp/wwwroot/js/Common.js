407a6d27d87fbe35dc3e4e83b5142d73bdfea580
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-09-19 18:50:47 -0500

    SAlert fail

--- BlazorLearn/BlazorApp/BlazorApp/wwwroot/js/Common.js
+++ BlazorLearn/BlazorApp/BlazorApp/wwwroot/js/Common.js
@@ -11,20 +11,57 @@
 
 }
 
-window.ShowSwal = function (type, message, title) {
+//window.ShowSwal = function (type, message, title) {
+//    if (type == "success") {
+//        Swal.fire({
+//            icon: 'success',
+//            title: title,
+//            text: message
+//        });
+//    }
+
+//    if (type == "error") {
+//        Swal.fire({
+//            icon: 'error',
+//            title: title,
+//            text: "Something is not valid"
+//        });
+//    }
+//}
+
+window.ShowSwal = function (type, message) {
     if (type == "success") {
-        Swlal.fire({
-            icon: 'success',
-            title: title,
+        Swal.fire({
+            icon: "success",
+            title: "title",
             text: message
         });
     }
 
     if (type == "error") {
-        Swlal.fire({
-            icon: 'error',
-            title: title,
+        Swal.fire({
+            icon: "error",
+            title: "title",
             text: "Something is not valid"
         });
+    }
+}
+
+window.showSwal2 = function (type, message, title) {
+    if (type == "success") {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        })
+    }
+    if (type == "error") {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        })
     }
 }
