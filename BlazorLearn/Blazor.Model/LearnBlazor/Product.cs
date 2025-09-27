8759583ddb64a9f56398d8fd67441d3e58d3a20c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2025-09-27 12:24:23 -0500

    adjust model product

--- BlazorLearn/Blazor.Model/LearnBlazor/Product.cs
+++ BlazorLearn/Blazor.Model/LearnBlazor/Product.cs
@@ -1,15 +1,35 @@
-﻿namespace Blazor.Model.LearnBlazor
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Model.LearnBlazor
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        [Range(1, 1000)]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
         public bool IsActive { get; set; }
         // Navigation property for related Product_Prop entities
         //public List<Product_Prop> Properties { get; set; } = new List<Product_Prop>();
         public IEnumerable<Product_Prop> ProductProperties { get; set; } = new List<Product_Prop>();
+
+        public Category Category { get; set; }
+        public DateOnly AvailableAfter { get; set; }
+    }
+
+    public enum Category
+    {
+        Entree,
+        Appetizer,
+        Dessert,
+        Electronics,
+        Books,
+        Clothing,
+        Home,
+        Sports
     }
 }
+
